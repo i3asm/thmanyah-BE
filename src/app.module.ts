923f606ec598422592d7422fe16a20b3051a948e
i3asm <i3asmmail@gmail.com>
2025-07-22 14:06:48 +0300

    ADD: docker and env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PodcastsModule } from './podcasts/podcasts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    PodcastsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    // the only reason this is here is that I want to test using env vars for both local dev and docker containers.
+    const port: string = this.configService.get<string>('PORT', '3000');
+    const env: string = this.configService.get<string>('NODE_ENV', 'development');
+
+    return `Hello World! Running on port ${port} in ${env} mode`;
   }
 }
