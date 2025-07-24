17dfa0a0663d9c8fac1b8293f4325028911a92da
i3asm <i3asmmail@gmail.com>
2025-07-25 01:38:04 +0300

    FIX: recognize module entities

--- src/podcasts/podcasts.module.ts
+++ src/podcasts/podcasts.module.ts
@@ -2,10 +2,14 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { PodcastsService } from './podcasts.service';
 import { PodcastsController } from './podcasts.controller';
 import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Genre, Program } from './entities/Program.entity';
+import { Episode } from './entities/Episode.entity';
+import { Cache } from './entities/Cache.entity';
 
 @Module({
   controllers: [PodcastsController],
   providers: [PodcastsService, ClassSerializerInterceptor],
-  imports: [HttpModule],
+  imports: [HttpModule, TypeOrmModule.forFeature([Program, Episode, Genre, Cache])],
 })
 export class PodcastsModule {}
